dbd68393393e610a0cbd7b3e77f133643219bf60
Trenden Leonard <trendenleonard@Trendens-MacBook-Air.local>
2021-06-25 21:54:11 -0500

    new-comp

--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Comp1 from "./ Comp1";
+import NewComp from "./NewComp";
 
 function App() {
   return (
     <div>
       <Comp1 />
       <h1>Hello World</h1>
+      <NewComp />
     </div>
   );
 }
---
+++ test/src/NewComp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class NewComp extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>New Component</h1>
+            </div>
+        )
+    }
+}
+
+export default NewComp
