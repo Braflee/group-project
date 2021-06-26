54d869a21968c526caa2e23d8fcd957051b524bc
Trenden Leonard <trendenleonard@Trendens-MacBook-Air.local>
2021-06-26 16:09:41 -0500

    business-logic

---
+++ meme-gen/src/components/States.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+class States extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            topTex: "",
+            bottomTex: "",
+            randomImg: "",
+            memeArray: []
+        }
+
+        this.shuffleButton = this.shuffleButton.bind(this)
+    }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(res => {
+                const {memes} = res.data
+                console.log(memes)
+                this.setState({ memeArray: memes })
+            })
+    }
+
+    shuffleButton = e => {
+        e.preventDefault()
+        const randomIndex = Math.floor(Math.random() * this.state.memeArray.length)
+        const randMemeImg = this.state.memeArray[randomIndex].url
+        this.setState({randomImg: randMemeImg})
+    }
+
+    render(){
+        
+        return(
+            <div>
+                <form>
+                    <button onClick={this.shuffleButton}>Shuffle</button>
+                    <img src={this.state.randomImg}/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default States
