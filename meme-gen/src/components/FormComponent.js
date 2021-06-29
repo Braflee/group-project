9a9e63d0023b5bd194d1d7a4f127869608cf0bda
Braflee <brad.duffy24@gmail.com>
2021-06-29 00:48:03 -0700

    resolved submit button and render-on-mount issue

--- meme-gen/src/components/FormComponent.js
+++ meme-gen/src/components/FormComponent.js
@@ -9,6 +9,7 @@ function FormComponent(props) {
           value={props.topText}
           onChange={props.handleChange}
           placeholder="Enter text"
+          maxLength='20'
         />
         <br />
         <input
@@ -16,17 +17,18 @@ function FormComponent(props) {
           value={props.bottomText}
           onChange={props.handleChange}
           placeholder="Enter more text"
+          maxLength='20'
         />
         <br />
         <button onClick={props.shuffleButton}>Refresh Meme Image</button>
-        <button type="submit" onSubmit={props.handleSubmit}>
+        <button onClick={props.handleSubmit}>
           Submit
         </button>
       </form>
       <div className="imgCont">
         <div
-          className="randomImg"
-          style={{ backgroundImage: `url(${props.randomImg})` }}
+          className="imgUrl"
+          style={{ backgroundImage: `url(${props.imgUrl})` }}
         >
           <h1>{props.topText}</h1>
           <h1>{props.bottomText}</h1>
