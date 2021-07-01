fc7507e9e46b98254b01b83b28c6dd99a91db76f
Braflee <brad.duffy24@gmail.com>
2021-06-30 18:40:49 -0700

    issue #35.2 work in progress

--- meme-gen/src/components/FormComponent.js
+++ meme-gen/src/components/FormComponent.js
@@ -2,23 +2,23 @@ import React from "react";
 
 function FormComponent(props) {
   return (
-    <div>
+    <div className='formDisp'>
       <form>
         <input
           name="topText"
           value={props.topText}
           onChange={props.handleChange}
-          placeholder="Enter text"
+          placeholder="Top"
         />
         <br />
         <input
           name="bottomText"
           value={props.bottomText}
           onChange={props.handleChange}
-          placeholder="Enter more text"
+          placeholder="Bottom"
         />
         <br />
-        <button
+        <button className='submitBtn'
           // Anonymous (helper) function to pass URL
           onClick={(e) => {
             e.preventDefault();
@@ -28,27 +28,29 @@ function FormComponent(props) {
           Submit
         </button>
       </form>
-      <div className="imgCont">
-        <button
-          // Anonymous (helper) function to smoothly pass target
-          onClick={(e) => {
-            e.preventDefault();
-            props.shuffleButton();
-          }}
-        >
-          Refresh Meme Image
-        </button>
-        <div
-          className="imgUrl"
-          style={{ backgroundImage: `url(${props.imgUrl.url})` }}
-        >
-          <div className="formTopText">
-            <h1>{props.topText}</h1>
+      <div className='imgDisp'>
+        <div className="imgCont">
+          <button className='refreshBtn'
+            // Anonymous (helper) function to smoothly pass target
+            onClick={(e) => {
+              e.preventDefault();
+              props.shuffleButton();
+            }}
+            >
+              Refresh
+          </button>
+            <div
+              className="imgUrl"
+              style={{ backgroundImage: `url(${props.imgUrl.url})` }}
+              >
+              <div className="formTopText">
+                <h1>{props.topText}</h1>
+              </div>
+              <div className="formBottomText">
+                <h1>{props.bottomText}</h1>
+              </div>
+            </div>
           </div>
-          <div className="formBottomText">
-            <h1>{props.bottomText}</h1>
-          </div>
-        </div>
       </div>
     </div>
   );
--- meme-gen/src/components/Memes.js
+++ meme-gen/src/components/Memes.js
@@ -4,25 +4,31 @@ export default function Memes(props) {
   const { imgUrl, topText, bottomText, id } = props.item; // Deconstructing
   // console.log(props.item);
   return (
-    <div>
-      <div className="userMeme" style={{ backgroundImage: `url(${imgUrl})` }}>
-        <div className="memeTopText">
-          <p>{topText}</p>
+    <div className='memeCont'>
+        <div className="userMeme" style={{ backgroundImage: `url(${imgUrl})` }}>
+          <div className="memeTopText">
+            <p>{topText}</p>
+          </div>
+          <div className="memeBottomText">
+            <p>{bottomText}</p>
+          </div>
         </div>
-        <div className="memeBottomText">
-          <p>{bottomText}</p>
+        <div>
+          <button className='deleteBtn'
+            // Anonymous (helper)) function to pass ID
+            onClick={(e) => {
+              e.preventDefault();
+              props.handleDelete(id);
+            }}
+          >
+            Delete
+          </button>
         </div>
-      </div>
-      <button
-        // Anonymous (helper)) function to pass ID
-        onClick={(e) => {
-          e.preventDefault();
-          props.handleDelete(id);
-        }}
-      >
-        Delete
-      </button>
-      <button onClick={props.handleEdit}>Edit</button>
+        <div>
+          <button className='editBtn' onClick={props.handleEdit}>
+            Edit
+          </button>
+        </div>
     </div>
   );
 }
